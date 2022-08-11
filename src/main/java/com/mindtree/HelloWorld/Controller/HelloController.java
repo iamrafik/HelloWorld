dad8b9e37e9cacadbdf262851fe6d5aceb2699b5
Rafik <hellorafik4@gmail.com>
2022-08-11 11:16:31 +0530

    First commit

---
+++ src/main/java/com/mindtree/HelloWorld/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.mindtree.HelloWorld.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello World!!";
+	}
+
+}
